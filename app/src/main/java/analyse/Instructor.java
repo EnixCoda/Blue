196f753c65fd4e17679e7002c24ca8579d6a6bdf
Exin <ggaao1994@gmail.com>
2016-06-19 15:37:17 +0800

    更新Day数据格式-临时

--- app/src/main/java/analyse/Instructor.java
+++ app/src/main/java/analyse/Instructor.java
@@ -4,24 +4,39 @@ import java.util.Hashtable;
 
 import getData.Day;
 
+
 public class Instructor {
+    private class Suggestion {
 
-    String outSuggestion;
+        String desc;
+        int code;
+
+        Suggestion(String s, int j) {
+            desc = s;
+            code = j;
+        }
+    }
+
+    String outSuggestion = "适宜出行";//-1 1
     String clothes;
-    String mask = "不需口罩";
-    String oldAndChild = "可以出行";
-    String dryClothes = "适合晾晒";
-    String airCleaner = "不需开启";
-    boolean ifOut = true;
-    boolean ifMask = false;
-    boolean ifOldAndChild = true;
-    boolean ifDryClothes = false;
-    boolean ifAirCleaner = false;
+    String umbrella = "不需带伞";//-1 1
+    String sports = "适合运动";//-1 1
+    String washCars = "适宜洗车";//-1 1
+    String airCleaner = "不需开启";//-1 1
+    String feeling = "体感一般";//-1 0 1
+    String cold = "感冒不易发";//-1 1
+    int ifOut = 1;
+    int ifUmbrella = -1;
+    int ifSports = 1;
+    int ifWashCars = 1;
+    int ifAirCleaner = -1;
+    int ifCold = -1;
+    int ifFeeling = 0;
 
-    Hashtable<String, Integer> pollutionNameToIndex;
-
-    String[] RespirableParticulateMatters = {"PM2.5", "PM10"};
-    String[] GaseousPollutants = {"O3", "NO2", "SO2", "CO"};
+    Hashtable<String, Integer> pollutionNameToIndex;//用哈希表来存放空气质量信息各标准的名字
+    String[] RespirableParticulateMatters = {"PM2.5", "PM10"};//可吸入颗粒物
+    String[] GaseousPollutants = {"O3", "NO2", "SO2", "CO"};//气体污染物
+    Hashtable<String, Suggestion> instructions;
 
     public Instructor() {
         pollutionNameToIndex = new Hashtable<>();
@@ -38,52 +53,99 @@ public class Instructor {
         pollutionNameToIndex.put("风", 11);
     }
 
-    public Hashtable<String, String> getInstructions(Day day) {
+    public String tellMeAboutAQI (int aqi) {
+        String msg = "空气质量";
+        if (aqi < 50) {
+            msg += "优良";
+        } else if (aqi < 100) {
+            msg += "尚可";
+        } else if (aqi < 150) {
+            msg += "较差";
+        } else {
+            msg += "极差";
+        }
+        return msg;
+    }
+
+    //根据确定的Day对象获得空气质量信息并返回建议，保存在哈希表中
+    public Hashtable<String, Suggestion> getInstructions(Day day) {
+        if (ifOutside("湿度", day.hourlyForecasts.get(0).humidity) == 1 && ifOutside("温度", day.hourlyForecasts.get(0).temp) == 1) {
+            ifFeeling = 1;
+            feeling = "体感舒适";
+        } else if (ifOutside("温度", day.hourlyForecasts.get(0).temp) != -1) {
+            ifFeeling = 0;
+            feeling = "体感一般";
+        } else {
+            ifFeeling = -1;
+            feeling = "体感不适";
+        }
+        //验证可吸入颗粒物
         for (String pollution : RespirableParticulateMatters) {
-            if (!ifOutside(pollution, day.stringIAQIHashtable.get(pollution).cur)) {
-                ifOut = false;
-                ifMask = true;
-                ifOldAndChild = false;
-                ifAirCleaner = true;
-                mask = "建议佩戴口罩";
+            if (ifOutside(pollution, day.stringIAQIHashtable.get(pollution).cur) == -1) {
+                ifOut = -1;
+                ifWashCars = -1;
+                ifSports = -1;
+                ifAirCleaner = 1;
+                ifFeeling = -1;
+                outSuggestion = "不宜出行";
+                washCars = "不宜洗车";
                 airCleaner = "建议开启";
-                oldAndChild = "不建议出行";
+                sports = "不宜运动";
+                feeling = "体感不适";
             }
         }
+        //验证空气污染物
         for (String pollution : GaseousPollutants) {
-            if (!ifOutside(pollution, day.stringIAQIHashtable.get(pollution).cur)) {
-                ifOut = false;
-                ifOldAndChild = false;
-                ifAirCleaner = true;
+            if (ifOutside(pollution, day.stringIAQIHashtable.get(pollution).cur) == -1) {
+                ifOut = -1;
+                ifWashCars = -1;
+                ifSports = -1;
+                ifAirCleaner = 1;
+                ifFeeling = -1;
+                outSuggestion = "不宜出行";
+                washCars = "不宜洗车";
                 airCleaner = "建议开启";
-                oldAndChild = "不建议出行";
+                sports = "不宜运动";
+                feeling = "体感不适";
             }
         }
-        if (!ifOutside("温度", day.hourlyForecasts.get(0).temp)) {
-            ifOut = false;
-            ifOldAndChild = false;
-            oldAndChild = "不建议出行";
-        }
-        if (!ifOutside("湿度", day.hourlyForecasts.get(0).humidity)) {
-            ifDryClothes = false;
-            dryClothes = "不适合晾晒";
-        }
-        if (!ifOutside("风", day.forecastWind.get(0).max)) {
-            ifOut = false;
-            ifOldAndChild = false;
-            ifDryClothes = false;
-            oldAndChild = "不建议出行";
-            dryClothes = "不适合晾晒";
-        }
-        if (day.hourlyForecasts.get(0).rainPoss > 50) {
-            ifOut = false;
-            ifOldAndChild = false;
-            ifDryClothes = false;
-            oldAndChild = "不建议出行";
-            dryClothes = "不适合晾晒";
+        //验证温度
+        if (ifOutside("温度", day.hourlyForecasts.get(0).temp) == -1) {
+            ifOut = -1;
+            ifSports = -1;
+            ifFeeling = -1;
+            outSuggestion = "不宜出行";
+            sports = "不宜运动";
+            feeling = "体感不适";
         }
 
-        if (ifOut) {
+        //验证风力
+        if (ifOutside("风", day.forecastWind.get(0).max) == -1) {
+            ifOut = -1;
+            ifSports = -1;
+            ifWashCars = -1;
+            ifFeeling = -1;
+            outSuggestion = "不宜出行";
+            sports = "不宜运动";
+            washCars = "不宜洗车";
+            feeling = "体感不适";
+        }
+        //验证降雨可能
+        if (day.hourlyForecasts.get(0).rainPoss > 50) {
+            ifOut = -1;
+            ifSports = -1;
+            ifWashCars = -1;
+            ifUmbrella = 1;
+            outSuggestion = "不宜出行";
+            sports = "不宜运动";
+            washCars = "不宜洗车";
+            umbrella = "建议带伞";
+        }
+        if ((day.stringIAQIHashtable.get("温度").max - day.stringIAQIHashtable.get("温度").min) > 15) {
+            ifCold = -1;
+            cold = "感冒易发";
+        }
+        /* if (ifOut) {
             for (String pollution : RespirableParticulateMatters) {
                 outSuggestion = getInstruction(pollution, day.stringIAQIHashtable.get(pollution).cur);
             }
@@ -91,37 +153,72 @@ public class Instructor {
             for (String pollution : RespirableParticulateMatters) {
                 outSuggestion = "建议避免出行";
             }
-        }
+        }*/
         clothes = getInstruction("温度", day.hourlyForecasts.get(0).temp);
 
-        Hashtable<String, String> instructions = new Hashtable<>();
-        instructions.put("outSuggestion", outSuggestion);
-        instructions.put("clothes", clothes);
-        instructions.put("mask", mask);
-        instructions.put("oldAndChild", oldAndChild);
-        instructions.put("dryClothes", dryClothes);
-        instructions.put("airCleaner", airCleaner);
+        Suggestion o = new Suggestion(outSuggestion, ifOut);
+        Suggestion c = new Suggestion(clothes, 0);
+        Suggestion u = new Suggestion(umbrella, ifUmbrella);
+        Suggestion s = new Suggestion(sports, ifSports);
+        Suggestion w = new Suggestion(washCars, ifWashCars);
+        Suggestion a = new Suggestion(airCleaner, ifAirCleaner);
+        Suggestion f = new Suggestion(feeling, ifFeeling);
+        Suggestion co = new Suggestion(cold, ifCold);
+
+        instructions = new Hashtable<>();
+        instructions.put("outSuggestion", o);
+        instructions.put("clothes", c);
+        instructions.put("cold", co);
+        instructions.put("umbrella", u);
+        instructions.put("sports", s);
+        instructions.put("washCars", w);
+        instructions.put("airCleaner", a);
+        instructions.put("feeling", f);
         return instructions;
     }
 
-    public boolean ifOutside(String type, double val) {
+    //验证用方法，输入空气质量信息返回是否建议出门
+    public int ifOutside(String type, double val) {
         switch (pollutionNameToIndex.get(type)) {
             case 1:
-                return val < 150;
+                if (val < 150) {
+                    return 1;
+                }
+                return -1;
             case 2:
                 /*if()
                 else ;*/
-                return val < 150;
+                if (val < 150) {
+                    return 1;
+                }
+                return -1;
             case 3:
-                return val < 200;
+                if (val < 200) {
+                    return 1;
+                }
+                return -1;
             case 4:
-                return val < 200;
+                if (val < 200) {
+                    return 1;
+                }
+                return -1;
             case 5:
-                return val < 150;
+                if (val < 200) {
+                    return 1;
+                }
+                return -1;
             case 6:
-                return val < 50;
+                if (val < 50) {
+                    return 1;
+                }
+                return -1;
             case 7:
-                return -20 < val && val < 40;
+                if (15 <= val && val <= 25) {
+                    return 1;
+                } else if (-15 < val && val < 30) {
+                    return 0;
+                }
+                return -1;
             case 8:
             /*if()
                 else ;*/
@@ -129,112 +226,62 @@ public class Instructor {
             /*if()
                 else ;*/
             case 10:
-                return val < 80;
+                if (40 <= val && val <= 50) {
+                    return 1;
+                }
+                return -1;
             case 11:
                 /*if()
-                else ;*/
-                return val < 10;
+				else ;*/
+                if (val < 10) {
+                    return 1;
+                }
+                return -1;
             default:
-                return true;
+                return 1;
         }
     }
+//根据输入空气质量信息返回单条建议
 
     String getInstruction(String type, int val) {
         switch (pollutionNameToIndex.get(type)) {
             case 1:
-                if (val < 50) {
-                    return "建议进行户外运动";
-                } else if (val < 100) {
-                    return "可正常进行户外运动";
-                } else if (val < 150) {
-                    return "敏感人群应减少户外活动";
-                } else if (val < 200) {
-                    return "建议减少外出";
+                if (val < 150) {
+                    return "适宜出行";
                 }
-                return "建议避免外出";
+                return "不宜出行";
             case 2:
                 /*if()
 				else ;*/
-                if (val < 50) {
-                    return "建议进行户外运动";
-                } else if (val < 100) {
-                    return "可正常进行户外运动";
-                } else if (val < 150) {
-                    return "敏感人群应减少户外活动";
-                } else if (val < 200) {
-                    return "建议减少外出";
+                if (val < 150) {
+                    return "适宜出行";
                 }
-                return "建议避免外出";
+                return "不宜出行";
             case 3:
-                if (val < 160) {
-                    return "";
-                }
-                if (val < 200) {
-                    return "";
-                }
-                if (val < 300) {
-                    return "";
-                } else {
-                    return "";
-                }
+                return "";
 
             case 4:
-                if (val < 100) {
-                    return "";
-                }
-                if (val < 200) {
-                    return "";
-                }
-                if (val < 300) {
-                    return "";
-                }
                 return "";
 
             case 5:
-                if (val < 100) {
-                    return "";
-                }
-                if (val < 150) {
-                    return "";
-                }
-                if (val < 500) {
-                    return "";
-                }
-                if (val < 650) {
-                    return "";
-                }
                 return "";
             case 6:
-                if (val < 5) {
-                    return "";
-                }
-                if (val < 10) {
-                    return "";
-                }
-                if (val < 50) {
-                    return "";
-                }
-                if (val < 100) {
-                    return "";
-                } else {
-                    return "";
-                }
-
+                return "";
             case 7:
                 if (val < 0) {
-                    return "建议穿羽绒服类厚衣物";
+                    return "适合羽绒服类";
                 }
                 if (val < 10) {
-                    return "适合穿皮夹克，风衣";
+                    return "适合皮夹克，风衣";
                 }
                 if (val < 20) {
-                    return "适合长袖休闲装，运动服";
+                    return "适合长袖服装";
                 }
                 if (val < 30) {
-                    return "适合穿衬衫，T恤，短套装";
+                    return "适合衬衫，T恤";
                 }
 
-                return "适合穿短衣裤，短裙";
+                return "适合短衣裤短裙";
             case 8:
                 /*if()
 				else ;*/
@@ -250,15 +297,7 @@ public class Instructor {
             case 11:
                 /*if()
 				else ;*/
-                if (val < 5) {
-                    return "";
-                } else if (val < 10) {
-                    return "";
-                } else if (val < 20) {
-                    return "";
-                } else {
-                    return "";
-                }
+                return "";
             default:
                 return "";
         }
