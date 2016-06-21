e5bcc398dbcc5e021ff2f6d2de871b4d013ad4cf
Exin <ggaao1994@gmail.com>
2016-06-21 20:45:39 +0800

    粗糙的天气数据绑定

--- app/src/main/java/com/xxx/blue/WeatherDetailsActivity.java
+++ app/src/main/java/com/xxx/blue/WeatherDetailsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import getData.Day;
@@ -39,7 +40,22 @@ public class WeatherDetailsActivity extends AppCompatActivity {
         FetchData fetchData = new FetchData(new FetchData.AsyncResponse() {
             @Override
             public void processFinish(Day day) {
-                // TODO: operate day
+                TextView temperature_value = (TextView) findViewById(R.id.temperature_value);
+                temperature_value.setText(Integer.toString(day.stringIAQIHashtable.get("温度").cur) + "℃");
+                TextView wind_value = (TextView) findViewById(R.id.wind_value);
+                wind_value.setText(Integer.toString(day.stringIAQIHashtable.get("风").cur) + "m/s");
+                TextView pressure_value = (TextView) findViewById(R.id.pressure_value);
+                pressure_value.setText(Integer.toString(day.stringIAQIHashtable.get("空气压力").cur) + "kPa");
+                TextView humidity_value = (TextView) findViewById(R.id.humidity_value);
+                humidity_value.setText(Integer.toString(day.stringIAQIHashtable.get("湿度").cur) + "%");
+                TextView today_weather = (TextView) findViewById(R.id.today_weather);
+                today_weather.setText(day.dailyForecasts.get(0).description);
+                TextView today_weather_value = (TextView) findViewById(R.id.today_weather_value);
+                today_weather_value.setText(Integer.toString(day.dailyForecasts.get(0).tempMin) + "~" + Integer.toString(day.dailyForecasts.get(0).tempMin) + "℃");
+                TextView tomorrow_weather = (TextView) findViewById(R.id.tomorrow_weather);
+                tomorrow_weather.setText(day.dailyForecasts.get(1).description);
+                TextView tomorrow_weather_value = (TextView) findViewById(R.id.tomorrow_weather_value);
+                tomorrow_weather_value.setText(Integer.toString(day.dailyForecasts.get(1).tempMin) + "~" + Integer.toString(day.dailyForecasts.get(1).tempMin) + "℃");
             }
         });
         fetchData.execute(localLocation);
@@ -66,21 +82,21 @@ public class WeatherDetailsActivity extends AppCompatActivity {
         mGridWeatherEveryday.setAdapter(mAdapter);
 
         //gridView 每小时天气
-        ArrayList<WeatherEveryhourItem> hourmodels = new ArrayList<>();
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
-        hourmodels.add(new WeatherEveryhourItem());
+        ArrayList<WeatherEveryhourItem> hourModels = new ArrayList<>();
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
+        hourModels.add(new WeatherEveryhourItem());
 
-        int size = hourmodels.size();
+        int size = hourModels.size();
         int length = 100;
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -95,7 +111,7 @@ public class WeatherDetailsActivity extends AppCompatActivity {
         mGridWeatherEveryhour.setHorizontalSpacing(5); // 设置列表项水平间距
         mGridWeatherEveryhour.setStretchMode(GridView.NO_STRETCH);
         mGridWeatherEveryhour.setNumColumns(size); // 设置列数量=列表集合数
-        hourAdapter = new WeatherEveryhourItemAdapter(this, hourmodels);
+        hourAdapter = new WeatherEveryhourItemAdapter(this, hourModels);
         hourAdapter.setGridView(mGridWeatherEveryhour);
         mGridWeatherEveryhour.setAdapter(hourAdapter);
     }
